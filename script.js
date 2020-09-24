93f91f17c3b093a57cce69c59d9afbb0e2b3de53
Alexander <vozman@yandex.ru>
2020-09-24 15:06:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function readlink() {
   link = document.getElementById('link').value
   document.getElementById('video').setAttribute('src', link);
+  setQuery(link)
   return false
 }
+
+function setQuery(link) {
+  let urlParams = new URLSearchParams(window.location.search);
+  urlParams.set('link', link);
+  console.log(urlParams)
+  
+  history.pushState(null, null, "?"+urlParams.toString());
+
+
+
+}
+
+function readQuery() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const link = urlParams.get('link');
+  document.getElementById('video').setAttribute('src', link);
+  document.getElementById('link').value = link
+  
+}
+
+window.onload = readQuery
+
+
